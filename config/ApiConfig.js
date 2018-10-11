b531729601c6b4674730a64de7ebc705932906b8
youngboo <504142201@qq.com>
2018-10-11 15:36:52 +0800

    周切换图标更改

--- config/ApiConfig.js
+++ config/ApiConfig.js
@@ -1,13 +1,6 @@
 export const API =() =>{
   return wx.getStorageSync('API');
 }
-// export const SAPI = {
-//   login: `${prefix}/TokenAuth/Authenticate`,
-//   userInfo: `${prefix}/services/app/User/GetInfoAsync`,
-//   courses: `${prefix}/services/app/Attendance/SearchMyAttendanceRecordsAsync`,
-//   studentList: `${prefix}/services/app/Course/GetSubscribeAsync`,
-//   updateAttendance: `${prefix}/services/app/Attendance/CreateOrUpdateAttendanceRecordAsync`
-// }
 export const getApiByMode = (mode) => {
   let prefix = wx.getStorageSync('gateway').server + '/api';
   switch(mode) {
